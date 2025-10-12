7e825de7103586c906639cbeeb5f2a6bcabeab78
Ahmet N. Çeşmeci <152704386+acesmeci@users.noreply.github.com>
2025-10-12 23:15:44 +0200

    Delete tests/test_maft.py

--- tests/test_maft.py
+++
@@ -1,8 +0,0 @@
-from autora.theorist.maft import ExampleRegressor
-
-def test():
-    theorist = ExampleRegressor()
-    assert theorist is not None
-
-
-# Note: We encourage you to adjust this test and write more tests.
